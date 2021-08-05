89a6c0d0c7ea33861ee151131eab42af86f396c5
SameerInstacash <sameer.khan@getinstacash.in>
2021-08-05 10:10:09 +0530

    Initial Commit

---
+++ InstaCoverTests/InstaCoverTests.swift
@@ -0,0 +1,33 @@
+//
+//  InstaCoverTests.swift
+//  InstaCoverTests
+//
+//  Created by Sameer Khan on 05/08/21.
+//
+
+import XCTest
+@testable import InstaCover
+
+class InstaCoverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
